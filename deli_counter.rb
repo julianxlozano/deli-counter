b59d9761a9a5c01ccb68f31f0b55440f5244187b
julianxlozano <julianxlozano@gmail.com>
2021-02-22 18:10:06 -0600

    commit tree 59eac3c60bbabbed68071b5e1bbcfeb4fe723de1

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+require 'pry'
+
+katz_deli=[]
+
+def line(array)
+    
+    if array.size==0
+        puts "The line is currently empty."
+    elsif 
+      collected= array.collect.with_index do |person,index|
+            "#{index +1}. #{person}"
+             
+        end
+        string=collected.join(" ")
+       puts  string.prepend("The line is currently: ")
+        
+    end
+ 
+   
+
+end
+
+def take_a_number(line,name)
+    line.push(name)
+    puts "Welcome, #{name}. You are number #{line.size} in line."
+
+end
+
+def now_serving(array)
+    if array.size==0
+        puts "There is nobody waiting to be served!"
+    else
+    puts "Currently serving #{array[0]}."
+    array.shift
+    end
+end
